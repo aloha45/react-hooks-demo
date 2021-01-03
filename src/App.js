b1009e371f8c88779d9e76396517cd66998b5e20
Cory Spicer <corys4139@gmail.com>
2021-01-03 16:57:00 -0500

    test hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
 import './App.css';
 
 function App() {
+  // declare a new state variable, which we'll call 'count'
+
+  const [count, setCount] = useState(0);
+  const [fruit, setFruit] = useState('banana');
+  const [todos, setTodos] = useState([{ text: 'Learn Hooks '}])
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  })
+  
+
   return (
+    <React.Fragment>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>You have clicked {count} times.</p>
+      <button onClick={()=> setCount( count + 1 )}>Click Me</button>
+      
+      <p>Here is the list of fruit: {fruit} </p>
+      <p>Here are some todos:</p>
+      {todos.map((todo) =>
+        <li>{todo.text}</li>
+      )}
     </div>
+    </React.Fragment>
   );
 }
 
