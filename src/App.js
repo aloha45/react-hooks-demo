f4c900f6e6ac3093a3fe916aa8a45a633ebd853b
Cory Spicer <corys4139@gmail.com>
2021-01-03 18:48:58 -0500

    useState hook

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import logo from './logo.svg';
 import React, { useState, useEffect } from 'react'
 import './App.css';
 
+
 function App() {
   // declare a new state variable, which we'll call 'count'
 
   const [count, setCount] = useState(0);
-  const [fruit, setFruit] = useState('banana');
+  const [fruit, setFruit] = useState(['banana']);
   const [todos, setTodos] = useState([{ text: 'Learn Hooks '}])
 
   useEffect(() => {
     document.title = `You clicked ${count} times`;
   })
-  
+
+  function handleFruit()  {
+    fruit.push(fruit)
+  }
 
   return (
     <React.Fragment>
@@ -20,7 +24,18 @@ function App() {
       <p>You have clicked {count} times.</p>
       <button onClick={()=> setCount( count + 1 )}>Click Me</button>
       
-      <p>Here is the list of fruit: {fruit} </p>
+      <p>Here is the list of fruit:</p>
+      {fruit.map((fru) => 
+        <li>{fru}</li>
+      )}
+      Fruit: {fruit}
+      <br></br>
+      <form>
+        <input onChange={e => setFruit(e.target.value)} type="textbox"></input>
+        <button onClick={()=> 
+          handleFruit(fruit)
+        }>Submit</button>
+      </form>
       <p>Here are some todos:</p>
       {todos.map((todo) =>
         <li>{todo.text}</li>
